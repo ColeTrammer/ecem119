0d25ca220310e1004f697a5146b422920fd989dd
ColeTrammer <cole.trammer@gmail.com>
2022-10-31 23:23:58 -0700

    m3: implement a websocket graph visualizer

---
+++ m3/controller.js
@@ -0,0 +1,87 @@
+// based on the example on https://www.npmjs.com/package/@abandonware/noble
+
+const noble = require("@abandonware/noble");
+const express = require("express");
+const fs = require("fs");
+
+const WebSocket = require("ws");
+const WebSocketServer = WebSocket.WebSocketServer;
+
+const uuid_service = "1101";
+const uuid_values = ["2101", "2102", "2103"];
+
+noble.on("stateChange", async (state) => {
+    if (state === "poweredOn") {
+        console.log("start scanning");
+        await noble.startScanningAsync([uuid_service], false);
+    }
+});
+
+noble.on("discover", async (peripheral) => {
+    if (peripheral.advertisement.localName !== "Cole's Nano 33 IoT") {
+        return;
+    }
+    await noble.stopScanningAsync();
+    await peripheral.connectAsync();
+    const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(
+        [uuid_service],
+        uuid_values
+    );
+    readData("ax", characteristics[0]);
+    readData("ay", characteristics[1]);
+    readData("az", characteristics[2]);
+});
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//
+// read data periodically
+//
+let readData = async (name, characteristic) => {
+    const value = await characteristic.readAsync();
+    console.log(`${name}: ${value.readFloatLE(0)}`);
+
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send({
+                name,
+                value: value.readFloatLE(0),
+                timestamp: new Date().toISOString(),
+            });
+        }
+    });
+
+    // read data again in t milliseconds
+    setTimeout(() => {
+        readData(name, characteristic);
+    }, 10);
+};
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/graph.js", (req, res) => {
+    res.sendFile(__dirname + "/graph.js");
+});
+
+app.listen(port, () => {
+    console.log(`Server started @ http://localhost:${port}`);
+});
+
+// setInterval(() => {
+//     wss.clients.forEach((client) => {
+//         if (client.readyState === WebSocket.OPEN) {
+//             client.send(
+//                 JSON.stringify({
+//                     name: ["x", "y", "z"][Math.floor(Math.random() * 3)],
+//                     value: (Math.random() - 0.5) * 6,
+//                     timestamp: new Date().toISOString(),
+//                 })
+//             );
+//         }
+//     });
+// }, 100);
---
+++ m3/graph.js
@@ -0,0 +1,84 @@
+const xContext = document.getElementById("imu-x").getContext("2d");
+const xData = [];
+const yData = [];
+const zData = [];
+const xCol = "rgb(100, 149, 237)";
+const yCol = "rgb(233, 116, 81)";
+const zCol = "rgb(9, 121, 105)";
+const color = Chart.helpers.color;
+const chart = new Chart(xContext, {
+    type: "line",
+    data: {
+        labels: ["ax", "ay", "az"],
+        datasets: [
+            {
+                label: "ax",
+                backgroundColor: color(xCol).alpha(0.5).rgbString(),
+                borderColor: xCol,
+                data: xData,
+            },
+            {
+                label: "ay",
+                backgroundColor: color(yCol).alpha(0.5).rgbString(),
+                borderColor: yCol,
+                data: yData,
+            },
+            {
+                label: "az",
+                backgroundColor: color(zCol).alpha(0.5).rgbString(),
+                borderColor: zCol,
+                data: zData,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            x: {
+                type: "realtime",
+                realtime: {
+                    duration: 20000,
+                },
+            },
+            y: {
+                stacked: true,
+                min: -6.0,
+                max: 6.0,
+            },
+        },
+        responsive: true,
+    },
+});
+
+const ws = new WebSocket("ws://localhost:8080");
+
+ws.addEventListener("message", (event) => {
+    const data = JSON.parse(event.data);
+    const dataset = (() => {
+        if (data.name === "x") {
+            return xData;
+        } else if (data.name === "y") {
+            return yData;
+        } else {
+            return zData;
+        }
+    })();
+    dataset.push({
+        x: new Date(data.timestamp),
+        y: data.value,
+    });
+
+    console.log(data);
+
+    chart.update("quiet");
+});
+
+// setInterval(() => {
+//     let i = 0;
+//     console.log(xChart);
+//     xData.push({
+//         x: Date.now(),
+//         y: Math.random(),
+//     });
+
+//     xChart.update("quiet");
+// }, 1000);
